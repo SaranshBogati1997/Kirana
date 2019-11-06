0c47e2696d746a57512484be8ffc10b35a875a8c
Saransh Bogati <50762571+SaranshBogati1997@users.noreply.github.com>
2019-11-06 11:37:39 +0545

    Models for the project and repositories built

---
+++ KiranaPasal/Kirana.Core/Models/Products.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kirana.Core.Models
+{
+    public class Products
+    {
+        //Products properties definition
+        public string Id { get; set; }
+
+        //validation added to Name's Max length and display when scaffolding
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string description { get; set; }
+
+        //Range validation on price
+        [Range(0,10000)]
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Image { get; set; }
+
+        public Products()//Constructor to initialize the id 
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ KiranaPasal/Kirana.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using Kirana.Core;
+using Kirana.Core.Models;
+
+namespace Kirana.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;//creating cache to store data in memory
+
+        List<Products> products;//init list of products 
+
+        public ProductRepository()//constructor to check for in memory product on each run
+        {
+            products = cache["products"] as List<Products>;//if there is cache called products then return as list of products
+            if(products == null)//if null init products as list
+            {
+                products = new List<Products>();
+            }
+        }
+
+        public void Commit()//saves data in products object to in memory cache before saving in server
+        {
+            cache["products"] = products;
+        }
+        public void Insert(Products p)
+        {
+            products.Add(p);
+        }
+
+        public void Update( Products prod)
+        {
+            Products ProductToUpdate = products.Find(p => p.Id == prod.Id);//Search through list based on id
+
+            if(ProductToUpdate != null)//if non empty then store product data on productToUpdate object
+            {
+                ProductToUpdate = prod;
+            }
+            else
+            {
+                throw new Exception("Product not found");//else throw exception
+            };
+        }
+
+        public Products Find(String Id)
+        {
+            Products ProductToFind = products.Find(p => p.Id == Id);//Search through list based on id
+
+            if (ProductToFind != null)//if non empty then return productToFind
+            {
+                return ProductToFind;
+            }
+            else
+            {
+                throw new Exception("Product not found");//else throw exception
+            };
+        }
+
+        public  IQueryable<Products> Collection()//returns list of product. this will return a list that can be queryed
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete(String Id)
+        {   
+            Products ProductToDelete = products.Find(p => p.Id == Id);//search through the list based on ID.
+
+            if( ProductToDelete != null)
+            {
+                products.Remove(ProductToDelete);//if product found on list will delete the list.
+            }
+            else
+            {
+                throw new Exception("Unable to find Product");//else will throw an exception.
+            }
+        }
+    }
+}
